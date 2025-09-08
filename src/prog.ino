7afe51d53b5d0a2965c8cd586b247241d06888bb
twichhs <leobarroscunha@gmail.com>
2025-09-08 20:46:34 -0300

    sitema_led

--- src/prog.ino
+++ src/prog.ino
@@ -1,10 +1,17 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
+  pinMode(23, OUTPUT); // Configura o pino 23 como saída
+  pinMode(22, OUTPUT); // Configura o pino 23 como saída
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(10); // this speeds up the simulation
+  digitalWrite(23, HIGH);
+  digitalWrite(22, LOW);
+  delay(1000);
+  digitalWrite(23, LOW);
+  digitalWrite(22, HIGH);
+  delay(1000);
+  Serial.println("Cuidado, carros.");
 }
+
