c639bbc580e742056cf5b54e2a1e8cd62e14a9d3
twichhs <leobarroscunha@gmail.com>
2025-09-15 21:37:02 -0300

    novas

--- src/prog.ino
+++ src/prog.ino
@@ -1,17 +1,11 @@
+
+char letra = "A";
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
-  pinMode(23, OUTPUT); // Configura o pino 23 como saída
-  pinMode(22, OUTPUT); // Configura o pino 23 como saída
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(23, HIGH);
-  digitalWrite(22, LOW);
+  Serial.println(letra);
   delay(1000);
-  digitalWrite(23, LOW);
-  digitalWrite(22, HIGH);
-  delay(1000);
-  Serial.println("Cuidado, carros.");
 }
-
